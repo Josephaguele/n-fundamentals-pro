6fc0474e57e1b94fe69202eea046c02836b1812a
Joseph Aguele <osekuemen@gmailcom>
2024-04-12 13:13:35 +0100

    make artists strings, and injecting object as a provider and used as a dependency

---
+++ src/common/constants/connection.ts
@@ -0,0 +1,10 @@
+export const connection: Connection = {
+  CONNECTION_STRING: 'MYSQL_DUMMY_CONNECTION_STRING',
+  DB: 'MYSQL',
+  DBNAME: 'TEST',
+};
+export type Connection = {
+  CONNECTION_STRING: string;
+  DB: string;
+  DBNAME: string;
+};
